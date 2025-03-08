d735f19f74e2bac63ed8141ca21942787a9e7c9e
zoheb-akhtar <akhtar.zo@northeastern.edu>
2025-03-08 02:05:14 -0500

    implementing nav

--- personal-portfolio/src/components/Sidebar/index.jsx
+++ personal-portfolio/src/components/Sidebar/index.jsx
@@ -1,15 +1,36 @@
 import "./index.scss";
-import { Link } from "react-router-dom"
-import {LogoS} from "../../assets/images/logo-s.png"
-import {LogoSub} from "../../assets/images/logo_sub.png";
+import { Link, NavLink } from "react-router-dom"
+import LogoZ from "../../assets/images/logo-z.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope, faHome, faUser, faLinkedin } from "@fortawesome/free-solid-svg-icons"
+
 
 export default function Sidebar() {
     return (
         <div className="nav-bar">
             <Link className="logo" to="/">
-                <img src={LogoS} alt="logo" />
-                <img src={LogoSub} alt="zoheb" />
+                <img src={LogoZ} alt="logo" />
             </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/zoheb-akhtar-593051284/">
+                        <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }

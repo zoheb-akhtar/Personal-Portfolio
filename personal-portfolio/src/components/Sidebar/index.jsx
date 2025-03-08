f9f7d1771e41c9ff76530ead5341802b10ad8aa2
zoheb-akhtar <akhtar.zo@northeastern.edu>
2025-03-08 01:20:47 -0500

    images

--- personal-portfolio/src/components/Sidebar/index.jsx
+++ personal-portfolio/src/components/Sidebar/index.jsx
@@ -1,7 +1,15 @@
 import "./index.scss";
+import { Link } from "react-router-dom"
+import {LogoS} from "../../assets/images/logo-s.png"
+import {LogoSub} from "../../assets/images/logo_sub.png";
 
 export default function Sidebar() {
     return (
-        <h1>Hello from sidebar</h1>
+        <div className="nav-bar">
+            <Link className="logo" to="/">
+                <img src={LogoS} alt="logo" />
+                <img src={LogoSub} alt="zoheb" />
+            </Link>
+        </div>
     )
 }
